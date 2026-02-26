715583d5ce476e00340250648375576622d0afd6
Kairavi Parmar <kairavi@biomedwaste.com>
2026-02-26 10:08:38 -0600

    updated codebase

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const msalConfig = {
     clientId: `bba349cc-ea7f-454b-b50c-8e5567009324`, 
     authority: `https://login.microsoftonline.com/8e46f333-ee3f-4f9b-aab9-36ab81142f78`, 
     redirectUri: `https://biomedwaste.net/auth/callback`,
-     //redirectUri: `http://localhost:3000/auth/callback`,
+    //redirectUri: `http://localhost:3000/auth/callback`,
   },
   cache: {
     cacheLocation: "sessionStorage",
